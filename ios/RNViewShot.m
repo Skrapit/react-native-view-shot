37f724393f082f1c9aa38cbc9cd75731aa34bac9
Joy <Joy@JoydeMacBook-Pro.local>
2017-03-21 16:21:38 +0800

    Decrease waiting time when snapshot area.

--- ios/RNViewShot.m
+++ ios/RNViewShot.m
@@ -367,7 +367,7 @@ RCT_EXPORT_METHOD(takeSnapshot:(nonnull NSNumber *)target
     
     UIGraphicsBeginImageContextWithOptions(target.frame.size, NO, [[UIScreen mainScreen] scale]);
     [target setContentOffset:CGPointMake(0, area.origin.y) animated:NO];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [target drawViewHierarchyInRect:target.frame afterScreenUpdates:YES];
         UIImage * capturedImage = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
